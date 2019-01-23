0bb25afcae750ea1a12ed927c110e8f9b6e0b87c
Shiva <shiva.sairam97@gmail.com>
2019-01-24 00:49:12 +0530

    Added native base to the project

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
-import { View , Text, Button } from 'react-native';
+import { View , Text } from 'react-native';
+import { Container, Header, Content, Button } from 'native-base';
 
 class ProfileScreen extends Component {
     
@@ -8,10 +9,18 @@ class ProfileScreen extends Component {
         
 
         return (
-            <View>
-                <Text>This is the profile screen</Text>
-                
-            </View>
+            <Container>
+            <Header />
+            <Content>
+              <Button light><Text> Light </Text></Button>
+              <Button primary><Text> Primary </Text></Button>
+              <Button success><Text> Success </Text></Button>
+              <Button info><Text> Info </Text></Button>
+              <Button warning><Text> Warning </Text></Button>
+              <Button danger><Text> Danger </Text></Button>
+              <Button dark><Text> Dark </Text></Button>
+            </Content>
+          </Container>
         );
     }
 }
