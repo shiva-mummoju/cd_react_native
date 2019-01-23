18ed4d52ed101f431bced5852820b95f8ec6ebf0
Shiva <shiva.sairam97@gmail.com>
2019-01-23 23:36:37 +0530

    Got vector icons working in android and ios

--- android/app/src/main/java/com/campusdiaries/MainActivity.java
+++ android/app/src/main/java/com/campusdiaries/MainActivity.java
@@ -3,11 +3,21 @@ package com.campusdiaries;
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 public class MainActivity extends NavigationActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
+//    @Override
+//    protected List<ReactPackage> getPackages() {
+//      return Arrays.<ReactPackage>asList(
+//        new MainReactPackage()
+//     , new VectorIconsPackage()
+//      );
+//    }
 
 }
